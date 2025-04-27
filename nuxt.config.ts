cf3c97614cc704ec0b1a1bfc5d45c3d959cff63a
SolsticeLeaf <me@sleaf.dev>
2025-04-27 14:58:25 +0300

    Changed icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,25 +1,26 @@
-const isDev = process.env.NODE_ENV === 'development'
+const isDev = process.env.NODE_ENV === "development";
 
 export default defineNuxtConfig({
   app: {
     pageTransition: false,
     layoutTransition: false,
     head: {
-      title: 'SLEAF | AUTH',
+      title: "SLEAF | AUTH",
       htmlAttrs: {
-        lang: 'en',
+        lang: "en",
       },
       meta: [
-        { charset: 'utf-8' },
+        { charset: "utf-8" },
         {
           name: "viewport",
-          content: "width=device-width, height=device-height, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0"
+          content:
+            "width=device-width, height=device-height, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0",
         },
-        { name: 'description', content: '' },
-        { name: 'format-detection', content: 'telephone=no' },
+        { name: "description", content: "" },
+        { name: "format-detection", content: "telephone=no" },
       ],
-      link: [{ rel: 'icon', type: 'image/x-icon', href: 'https://ik.imagekit.io/kiinse/icons/icon.svg?updatedAt=1740170186956' }]
-    }
+      link: [{ rel: "icon", type: "image/x-icon", href: "https://ik.imagekit.io/kiinse/sleaf-auth.png?updatedAt=1745755052715" }],
+    },
   },
   nitro: {
     compressPublicAssets: true,
@@ -29,47 +30,47 @@ export default defineNuxtConfig({
     renderJsonPayloads: true,
   },
   sourcemap: true,
-  compatibilityDate: '2025-01-29',
+  compatibilityDate: "2025-01-29",
   devtools: { enabled: true },
-  modules: ['@nuxtjs/i18n', '@nuxtjs/device', '@nuxt/icon', '@nuxt/fonts', '@nuxt/image', 'nuxt-umami', '@nuxtjs/color-mode'],
+  modules: ["@nuxtjs/i18n", "@nuxtjs/device", "@nuxt/icon", "@nuxt/fonts", "@nuxt/image", "nuxt-umami", "@nuxtjs/color-mode"],
   icon: {
     serverBundle: {
-      collections: ['pixelarticons']
-    }
+      collections: ["pixelarticons"],
+    },
   },
-  plugins: ['@/plugins/Vue3Marquee.client.ts'],
+  plugins: ["@/plugins/Vue3Marquee.client.ts"],
   i18n: {
     locales: [
-      { code: 'en', iso: 'en-US', name: 'English', file: 'en-US.json' },
-      { code: 'ru', iso: 'ru-RU', name: 'Русский', file: 'ru-RU.json' },
+      { code: "en", iso: "en-US", name: "English", file: "en-US.json" },
+      { code: "ru", iso: "ru-RU", name: "Русский", file: "ru-RU.json" },
     ],
     lazy: true,
-    langDir: 'locales',
+    langDir: "locales",
     bundle: {
-      optimizeTranslationDirective: false
+      optimizeTranslationDirective: false,
     },
     detectBrowserLanguage: {
       useCookie: true,
-      fallbackLocale: 'en',
+      fallbackLocale: "en",
     },
-    strategy: 'no_prefix',
-    defaultLocale: 'en',
+    strategy: "no_prefix",
+    defaultLocale: "en",
   },
   colorMode: {
-    preference: 'system',
-    fallback: 'light',
-    hid: 'nuxt-color-mode-script',
-    globalName: '__NUXT_COLOR_MODE__',
-    componentName: 'ColorScheme',
-    classPrefix: '',
-    classSuffix: '',
-    storage: 'localStorage',
-    storageKey: 'site-color-mode'
+    preference: "system",
+    fallback: "light",
+    hid: "nuxt-color-mode-script",
+    globalName: "__NUXT_COLOR_MODE__",
+    componentName: "ColorScheme",
+    classPrefix: "",
+    classSuffix: "",
+    storage: "localStorage",
+    storageKey: "site-color-mode",
   },
   devServer: {
     port: 4000,
   },
-  css: ['@/assets/scss/global.scss', '@/assets/scss/screens.scss'],
+  css: ["@/assets/scss/global.scss", "@/assets/scss/screens.scss"],
   umami: {
     id: process.env.UMAMI_ID,
     host: process.env.UMAMI_HOST,
@@ -89,6 +90,6 @@ export default defineNuxtConfig({
       EMAIL_SMTP_PASSWORD: process.env.EMAIL_SMTP_PASSWORD,
       UMAMI_ID: process.env.UMAMI_ID,
       UMAMI_HOST: process.env.UMAMI_HOST,
-    }
+    },
   },
-})
+});
